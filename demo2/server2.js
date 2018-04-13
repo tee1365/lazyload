3d7c584d6aec2e4cdcd0090415bae422d11485ba
tee1365 <31180529+tee1365@users.noreply.github.com>
2018-04-13 20:57:52 +1200

    Add files via upload

---
+++ demo2/server2.js
@@ -0,0 +1,32 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var path = require("path")
+
+http.createServer(function(req, res) {
+
+  var pathObj = url.parse(req.url, true)
+  console.log(pathObj);
+
+  if (pathObj.pathname == "/loadMore") {
+    var curIndex = pathObj.query.idx;
+    console.log("query:")
+    console.log(pathObj.query);
+    var len = pathObj.query.len;
+    var data = []
+
+    for (var i = 0; i < len; i++) {
+      data.push("content" + (parseInt(curIndex) + i))
+    }
+    res.end(JSON.stringify(data));
+  } else {
+    fs.readFile(path.join(__dirname, pathObj.pathname), function(err, data) {
+      if (err) {
+        res.statusCode = 404
+        res.end('Not found')
+      } else {
+        res.end(data)
+      }
+    })
+  }
+}).listen(8080);
